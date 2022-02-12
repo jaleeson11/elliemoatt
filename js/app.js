55cf59df9be325db9938e5b879231e41647f4e2f
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-02-12 14:47:34 +0000

    Added intersection observer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,2 @@
 import '../node_modules/bootstrap/dist/js/bootstrap.js';
+import './intersection-observer.js';
---
+++ js/intersection-observer.js
@@ -0,0 +1,21 @@
+const elements = document.querySelectorAll('.fade-in');
+
+const options = {
+	root: null,
+	threshold: 1,
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+	entries.forEach(entry => {
+		if(!entry.isIntersecting) {
+			return;
+		}
+		entry.target.classList.toggle('show');
+		observer.unobserve(entry.target);
+	})
+}, options);
+
+elements.forEach(element => {
+	observer.observe(element);
+});
+
