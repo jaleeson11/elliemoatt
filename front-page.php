f7a3c401ea82497bf7f929c448d548d024b96204
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-02-12 14:05:46 +0000

    Implemented button design

--- front-page.php
+++ front-page.php
@@ -19,6 +19,7 @@ get_header();
 							<h2>About Me</h2>
 							<p>Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.</p>
 							<p>Cras ultricies ligula sed magna dictum porta. Cras ultricies ligula sed magna dictum porta. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+							<a href="#" class="site-button">Button</a>
 						</div>
 					</div>
 					<div class="col-sm-12 col-md-6">
