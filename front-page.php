59975318c3e065c8d6ef4258fafdf405e32da743
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-02-12 12:34:28 +0000

    Implemented about section design

--- front-page.php
+++ front-page.php
@@ -11,6 +11,23 @@ get_header();
 	<main id="primary" class="site-main">
 		<div class="container">
 			<?php elliemoatt_hero( get_the_id() ); ?>
+
+			<section class="site-section site-section--about">
+				<div class="row align-items-center">
+					<div class="col-sm-12 col-md-6">
+						<div class="site-section__content">
+							<h2>About Me</h2>
+							<p>Vivamus suscipit tortor eget felis porttitor volutpat. Cras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.</p>
+							<p>Cras ultricies ligula sed magna dictum porta. Cras ultricies ligula sed magna dictum porta. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.</p>
+						</div>
+					</div>
+					<div class="col-sm-12 col-md-6">
+						<div class="site-section__image">
+							<img src="http://localhost/elliemoatt/wp-content/uploads/2022/02/mid-shot-woman-therapist-taking-notes-with-laptop-lap-scaled.jpg">
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
 	</main>
 
