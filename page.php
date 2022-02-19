3c1fac9101e517217e6f6b90a1a1b2c5de3c6d33
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-02-19 10:07:08 +0000

    Removed comments and added theme hero to generic page template

--- page.php
+++ page.php
@@ -17,24 +17,28 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<?php elliemoatt_hero( get_the_id() ); ?>
+		<div class="container">
+			<?php elliemoatt_hero( get_the_id() ); ?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
+			endwhile; // End of the loop.
+			?>
+		</div>
 
 	</main><!-- #main -->
 
+	<style>
+		<?php if ( get_field( 'banner_heading' ) ) : ?>
+		.page .entry-title {
+			display: none;
+		}
+		<?php endif; ?>
+	</style>
+
 <?php
-get_sidebar();
 get_footer();
