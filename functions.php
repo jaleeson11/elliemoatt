ef66cd1b85d30f4198e31053245bd26e9214a390
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2022-03-05 12:43:18 +0000

    Imported navigation js into main file

--- functions.php
+++ functions.php
@@ -140,8 +140,6 @@ function elliemoatt_scripts() {
 	wp_enqueue_style( 'elliemoatt-styles', get_template_directory_uri() . '/dist/style.css', array(), _S_VERSION );
 	wp_enqueue_script( 'elliemoatt-scripts', get_template_directory_uri() . '/dist/app.js', array( 'jquery' ), _S_VERSION, true );
 
-	wp_enqueue_script( 'elliemoatt-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
